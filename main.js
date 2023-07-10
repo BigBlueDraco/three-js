1a586108cde6a302803293493cd71f10d19cdc10
BigBlueDraco <Dafarlog@gmail.com>
2023-07-10 16:41:53 +0300

    Added camera return

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ import "./style.css";
 import * as THREE from "three";
 import { OBJLoader } from "three/addons/loaders/OBJLoader.js";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-let animId;
-let rabit;
+const cameraIdle = new THREE.Vector3(0, 350, 350);
+const currentCamera = new THREE.Vector3(0, 150, 150);
+let returnCameraId;
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
@@ -13,6 +15,8 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
+camera.position.setZ(cameraIdle.z);
+camera.position.setY(cameraIdle.y);
 
 const renderer = new THREE.WebGL1Renderer({
   canvas: document.querySelector("#bg"),
@@ -20,15 +24,16 @@ const renderer = new THREE.WebGL1Renderer({
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(200);
 
 const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(100, 1000, 0);
 
-pointLight.position.set(300, 500, 2000);
 scene.add(pointLight);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableZoom = false;
+controls.autoRotate = true;
+controls.autoRotateSpeed = -3;
 controls.enablePan = false;
 
 const loader = new OBJLoader();
@@ -39,10 +44,7 @@ loader.load(
     object.scale.x = 1000;
     object.scale.y = 1000;
     object.scale.z = 1000;
-    rabit = object;
-    scene.add(rabit);
-    anim();
-
+    scene.add(object);
     renderer.render(scene, camera);
   },
   function (xhr) {
@@ -52,24 +54,38 @@ loader.load(
     console.log(error);
   }
 );
-function stopAnimation(e) {
-  cancelAnimationFrame(animId);
-}
+
 function loop() {
   requestAnimationFrame(loop);
+
   controls.update();
   renderer.render(scene, camera);
 }
-function anim() {
-  cancelAnimationFrame(animId);
-  animId = requestAnimationFrame(() => anim());
-  rabit.rotation.y += 0.005;
-  renderer.render(scene, camera);
+function returnCamera(frame) {
+  returnCameraId = requestAnimationFrame(() => returnCamera(frame + 0.05));
+  if (frame > 1.5) {
+    camera.position.z = cameraIdle.z;
+    camera.position.y = cameraIdle.y;
+
+    console.log(camera.position.z);
+    cancelAnimationFrame(returnCameraId);
+    controls.autoRotate = true;
+  }
+  camera.position.lerpVectors(
+    currentCamera,
+    new THREE.Vector3(currentCamera.x, cameraIdle.y, currentCamera.z),
+    [frame]
+  );
 }
+function onControleRelise() {
+  cancelAnimationFrame(returnCameraId);
+  controls.autoRotate = false;
+  currentCamera.set(camera.position.x, camera.position.y, camera.position.z);
+  returnCamera(0);
+}
+
 loop();
-renderer.domElement.addEventListener("mousedown", stopAnimation);
-renderer.domElement.addEventListener("mouseup", () => {
-  setTimeout(() => {
-    anim();
-  }, 2000);
+renderer.domElement.addEventListener("mouseup", onControleRelise);
+renderer.domElement.addEventListener("mousedown", () => {
+  cancelAnimationFrame(returnCameraId);
 });
